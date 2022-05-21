0d0d6e52560092f25430517af78ca269d3d2756b
Gustavo Angelo da Silva <gustavo.silvaangelo@gmail.com>
2022-05-21 10:34:03 -0300

    Removendo códigos não usados

--- lib/rest/api.dart
+++ lib/rest/api.dart
@@ -1,3 +1,4 @@
 class API {
-  static const String endpoint = "Vendasspringappbeanstalk-env.eba-t9ewb6fp.us-east-2.elasticbeanstalk.com";
+  static const String endpoint = "10.0.2.2:8080";
+  //Vendasspringappbeanstalk-env.eba-t9ewb6fp.us-east-2.elasticbeanstalk.com
 }
